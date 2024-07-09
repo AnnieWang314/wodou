7dd63d852a982a014963c027626368d942340a14
Annie Wang <anniewang2023@gmail.com>
2024-07-08 21:06:12 -0700

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "wodou",
   description: "puzzle",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
